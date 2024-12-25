47a0b9ccfbb152c95954536d75391837e9ebc936
Kcity162 <kevin.torrington@gmail.com>
2024-12-25 23:44:43 +0000

    added tabs with icons

--- src/components/IconPositionTabs.js
+++ src/components/IconPositionTabs.js
@@ -5,6 +5,16 @@ import PhoneIcon from '@mui/icons-material/Phone';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import PersonPinIcon from '@mui/icons-material/PersonPin';
 import PhoneMissedIcon from '@mui/icons-material/PhoneMissed';
+import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import SportsBarIcon from '@mui/icons-material/SportsBar';
+import NewReleasesIcon from '@mui/icons-material/NewReleases';
+import SelfImprovementIcon from '@mui/icons-material/SelfImprovement';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import CastleIcon from '@mui/icons-material/Castle';
+import CrisisAlertIcon from '@mui/icons-material/CrisisAlert';
+import SportsMartialArtsIcon from '@mui/icons-material/SportsMartialArts';
 
 export default function IconPositionTabs() {
   const [value, setValue] = React.useState(0);
@@ -19,10 +29,18 @@ export default function IconPositionTabs() {
       onChange={handleChange}
       aria-label="icon position tabs example"
     >
-      <Tab icon={<PhoneIcon />} iconPosition="start" label="start"/>
-      <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="start" />
-      <Tab icon={<FavoriteIcon />} iconPosition="start" label="start" />
-      <Tab icon={<PersonPinIcon />} iconPosition="start" label="start" />
+      <Tab icon={<FavoriteIcon />} iconPosition="start" label="Fav's"/>
+      <Tab icon={<SportsMartialArtsIcon />} iconPosition="start" label="Combat" />
+      <Tab icon={<CrisisAlertIcon />} iconPosition="start" label="Suspenseful" />
+      <Tab icon={<CastleIcon />} iconPosition="start" label="Epic" />
+      <Tab icon={<AutoAwesomeIcon />} iconPosition="start" label="Mystical" />
+      <Tab icon={<SelfImprovementIcon />} iconPosition="start" label="Calm" />
+      <Tab icon={<NewReleasesIcon />} iconPosition="start" label="Chaotic" />
+      <Tab icon={<SportsBarIcon />} iconPosition="start" label="Tavern & Town" />
+      <Tab icon={<ThunderstormIcon />} iconPosition="start" label="Environmental" />
+      <Tab icon={<SentimentVeryDissatisfiedIcon />} iconPosition="start" label="Emotional" />
+      <Tab icon={<AutoFixHighIcon />} iconPosition="start" label="Spells" />
+
     </Tabs>
   );
 }
