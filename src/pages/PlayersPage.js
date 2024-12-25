d6c6d12d280e706fa10e9feb802c01b8197af515
Kcity162 <kevin.torrington@gmail.com>
2024-12-25 23:07:12 +0000

    Added page tabs

--- src/pages/PlayersPage.js
+++ src/pages/PlayersPage.js
@@ -4,9 +4,19 @@ import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import SoundCard from '../components/SoundCard';
+import { ToggleButtonGroup } from '@mui/material';
+import ColorToggleButton from '../components/IconPositionTabs';
+import IconPositionTabs from '../components/IconPositionTabs';
 
 export default function SoundTable() {
   return (
+      <>
+      
+      <Grid container spacing={1} justifyContent="flex-start" alignItems="center">
+        <Grid item xs={12} style={{paddingBottom: "5px"}}>
+        <IconPositionTabs/>
+          </Grid>
+      </Grid>
       <Grid container spacing={1} justifyContent="flex-start" alignItems="center">
         <Grid item xs={2}>
           <SoundCard title="Sound 1" description="Description for sound 1" />
@@ -50,7 +60,77 @@ export default function SoundTable() {
         <Grid item xs={2}>
           <SoundCard title="Sound 2" description="Description for sound 2" />
         </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
+        <Grid item xs={2}>
+          <SoundCard title="Sound 2" description="Description for sound 2" />
+        </Grid>
       </Grid>
+      </>
     );
              
 }
